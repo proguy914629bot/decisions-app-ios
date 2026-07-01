0583f4bc34d57c88fcbaab8eb85c570af6e53a12
proguy914629 <proguy@proguy.me>
2026-07-01 22:51:17 +0700

    Initial Commit

---
+++ Decisions/DecisionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecisionsApp.swift
+//  Decisions
+//
+//  Created by Mac on 01/07/26.
+//
+
+import SwiftUI
+
+@main
+struct DecisionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
